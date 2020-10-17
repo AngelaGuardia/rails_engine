03a3e1316c69bc89fb7cd50d5741b243f2f84063
Angela Guardia <angelaguardia20@gmail.com>
2020-10-17 16:14:32 -0700

    Update Customers table to have first and last name * Update migrations, spec, model, and schema

--- db/schema.rb
+++ db/schema.rb
@@ -10,15 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_10_17_230409) do
+ActiveRecord::Schema.define(version: 2020_10_17_231037) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "customers", force: :cascade do |t|
-    t.string "name"
+    t.string "first_name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "last_name"
   end
 
   create_table "items", force: :cascade do |t|
