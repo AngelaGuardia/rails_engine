fcb453bbc85f6a4dbcdb726bbc0bead49b546060
Angela Guardia <angelaguardia20@gmail.com>
2020-10-17 17:31:00 -0700

    Remove validation for credit_card_expiration_date * Raw data did not include values for this

--- app/models/transaction.rb
+++ app/models/transaction.rb
@@ -2,5 +2,5 @@ class Transaction < ApplicationRecord
   belongs_to :invoice
 
   validates :credit_card_number, presence: true, numericality: true
-  validates_presence_of :credit_card_expiration_date, :result
+  validates_presence_of :result
 end
