0df260388344118bbf29f2d78eeadb3515f0d1c0
Angela Guardia <angelaguardia20@gmail.com>
2020-10-17 17:31:00 -0700

    Change credit_card_number type to String

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_10_18_043355) do
+ActiveRecord::Schema.define(version: 2020_10_18_044337) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -61,7 +61,7 @@ ActiveRecord::Schema.define(version: 2020_10_18_043355) do
 
   create_table "transactions", force: :cascade do |t|
     t.bigint "invoice_id"
-    t.integer "credit_card_number"
+    t.string "credit_card_number"
     t.datetime "credit_card_expiration_date"
     t.string "result"
     t.datetime "created_at", null: false
