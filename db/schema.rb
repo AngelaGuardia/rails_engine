e19ac8040a253ea8f6267d1a9146d73a6c77dbc1
Angela Guardia <angelaguardia20@gmail.com>
2020-10-17 17:07:47 -0700

    Add Transaction Model * Add test, model, factory, migrations, and updated schema

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_10_17_231645) do
+ActiveRecord::Schema.define(version: 2020_10_18_000644) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -48,7 +48,16 @@ ActiveRecord::Schema.define(version: 2020_10_17_231645) do
     t.datetime "updated_at", null: false
   end
 
+  create_table "transactions", force: :cascade do |t|
+    t.bigint "invoice_id"
+    t.integer "credit_card_number"
+    t.datetime "credit_card_expiration_date"
+    t.string "result"
+    t.index ["invoice_id"], name: "index_transactions_on_invoice_id"
+  end
+
   add_foreign_key "invoices", "customers"
   add_foreign_key "invoices", "merchants"
   add_foreign_key "items", "merchants"
+  add_foreign_key "transactions", "invoices"
 end
