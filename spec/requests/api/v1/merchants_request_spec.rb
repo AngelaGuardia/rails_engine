0d6569beca4d0e130a6031e40180a178bd7b9294
Angela Guardia <angelaguardia20@gmail.com>
2020-10-19 15:44:32 -0700

    Refactor Merchants request spec to account for MerchantSerializer use

--- spec/requests/api/v1/merchants_request_spec.rb
+++ spec/requests/api/v1/merchants_request_spec.rb
@@ -1,37 +1,46 @@
 require 'rails_helper'
 
 describe "Merchants API" do
-  it "sends a list of merchants" do
-    create_list(:merchant, 3)
+  before :each do
+    create_list(:merchant, 2)
+    create_list(:item, 3, merchant_id: Merchant.first.id)
+    create_list(:item, 3, merchant_id: Merchant.last.id)
+    create_list(:customer, 2)
+    create_list(:invoice, 3, merchant_id: Merchant.first.id, customer_id: Customer.first.id)
+    create_list(:invoice, 3, merchant_id: Merchant.last.id, customer_id: Customer.last.id)
+  end
 
+  it "sends a list of merchants" do
     get '/api/v1/merchants'
     expect(response).to be_successful
 
-    merchants = JSON.parse(response.body, symbolize_names: true)
-    expect(merchants.count).to eq(3)
+    merchants_data = JSON.parse(response.body, symbolize_names: true)
 
-    merchants.each do |merchant|
-      expect(merchant).to have_key(:id)
-      expect(merchant[:id]).to be_an(Integer)
+    expect(merchants_data).to have_key(:data)
+    expect(merchants_data[:data]).to be_an(Array)
 
-      expect(merchant).to have_key(:name)
-      expect(merchant[:name]).to be_an(String)
-    end
+    merchants = merchants_data[:data]
+    merchant = merchants.first
+    serializer_structure_check(merchant)
+
+    expect(merchants.count).to eq(2)
   end
 
-  it "can show a specifi merchant by id" do
-    merchant = create(:merchant)
+  it "can show a specific merchant by id" do
+    merchant_object = Merchant.last
 
-    get "/api/v1/merchants/#{merchant.id}"
+    get "/api/v1/merchants/#{merchant_object.id}"
     expect(response).to be_successful
 
-    merchant = JSON.parse(response.body, symbolize_names: true)
+    merchants_data = JSON.parse(response.body, symbolize_names: true)
 
-    expect(merchant).to have_key(:id)
-    expect(merchant[:id]).to be_an(Integer)
+    expect(merchants_data).to have_key(:data)
+    expect(merchants_data[:data]).to be_an(Hash)
 
-    expect(merchant).to have_key(:name)
-    expect(merchant[:name]).to be_an(String)
+    merchant = merchants_data[:data]
+    serializer_structure_check(merchant)
+
+    expect(merchant[:attributes][:id]).to eq(merchant_object.id)
   end
 
   it "can create a new merchant" do
@@ -42,10 +51,17 @@ describe "Merchants API" do
     post '/api/v1/merchants', headers: headers, params: JSON.generate(merchant: merchant_params)
 
     expect(response).to be_successful
+    merchants_data = JSON.parse(response.body, symbolize_names: true)
 
-    merchant = Merchant.last
+    expect(merchants_data).to have_key(:data)
+    expect(merchants_data[:data]).to be_an(Hash)
 
-    expect(merchant.name).to eq(merchant_params[:name])
+    merchant = merchants_data[:data]
+    serializer_structure_check(merchant)
+
+    merchant_object = Merchant.last
+
+    expect(merchant_object.name).to eq(merchant_params[:name])
   end
 
   it "can update an existing merchant" do
@@ -56,22 +72,64 @@ describe "Merchants API" do
     headers = {"CONTENT_TYPE" => "application/json"}
 
     patch "/api/v1/merchants/#{id}", headers: headers, params: JSON.generate({merchant: merchant_params})
-    merchant = Merchant.find_by(id: id)
+
+    merchants_data = JSON.parse(response.body, symbolize_names: true)
+
+    expect(merchants_data).to have_key(:data)
+    expect(merchants_data[:data]).to be_an(Hash)
+
+    merchant = merchants_data[:data]
+    serializer_structure_check(merchant)
+
+    merchant_object = Merchant.find_by(id: id)
 
     expect(response).to be_successful
-    expect(merchant.name).to_not eq(previous_name)
-    expect(merchant.name).to eq("Sam's Club")
+    expect(merchant_object.name).to_not eq(previous_name)
+    expect(merchant_object.name).to eq("Sam's Club")
   end
 
   it "can destroy a merchant" do
-    merchant = create(:merchant)
-
-    expect(Merchant.count).to eq(1)
+    merchant = Merchant.last
+    expect(Merchant.count).to eq(2)
 
     delete "/api/v1/merchants/#{merchant.id}"
 
     expect(response).to be_successful
-    expect(Merchant.count).to eq(0)
+
+    expect(Merchant.count).to eq(1)
     expect{Merchant.find(merchant.id)}.to raise_error(ActiveRecord::RecordNotFound)
+
+    merchants_data = JSON.parse(response.body, symbolize_names: true)
+
+    expect(merchants_data).to have_key(:data)
+    expect(merchants_data[:data]).to be_an(Hash)
+
+    merchant = merchants_data[:data]
+    serializer_structure_check(merchant)
+  end
+
+  def serializer_structure_check(merchant)
+    expect(merchant).to have_key(:id)
+    expect(merchant[:id]).to be_a(String)
+
+    expect(merchant).to have_key(:type)
+    expect(merchant[:type]).to be_a(String)
+    expect(merchant[:type]).to eq("merchant")
+
+    expect(merchant).to have_key(:attributes)
+    expect(merchant[:attributes]).to be_a(Hash)
+
+    expect(merchant[:attributes]).to have_key(:name)
+    expect(merchant[:attributes][:name]).to be_a(String)
+
+    expect(merchant[:attributes]).to have_key(:id)
+    expect(merchant[:attributes][:id]).to be_an(Integer)
+
+    expect(merchant).to have_key(:relationships)
+    expect(merchant[:relationships]).to be_a(Hash)
+    expect(merchant[:relationships]).to have_key(:items)
+    expect(merchant[:relationships][:items]).to be_a(Hash)
+    expect(merchant[:relationships]).to have_key(:invoices)
+    expect(merchant[:relationships][:invoices]).to be_a(Hash)
   end
 end
