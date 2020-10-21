b7a5a2c96663b65b7b931b10f4755bd30f4623c6
Angela Guardia <angelaguardia20@gmail.com>
2020-10-21 11:35:39 -0700

    Create test for single_finder Merchant class method

--- spec/models/merchant_spec.rb
+++ spec/models/merchant_spec.rb
@@ -9,4 +9,23 @@ RSpec.describe Merchant, type: :model do
     it { should have_many :items }
     it { should have_many :invoices }
   end
+
+  describe 'class methods' do
+    it '#single_finder' do
+      merchant1 = Merchant.create(name: "King's shopper")
+      merchant2 = Merchant.create(name: "Queen's shopper")
+
+      result = Merchant.single_finder({name: 'King'})
+      expect(result.first).to eq(merchant1)
+
+      result = Merchant.single_finder({name: 'kInG'})
+      expect(result.first).to eq(merchant1)
+
+      result = Merchant.single_finder({name: 'queen'})
+      expect(result.first).to eq(merchant2)
+
+      result = Merchant.single_finder({name: 'shopper'})
+      expect(result.count).to eq(1)
+    end
+  end
 end
