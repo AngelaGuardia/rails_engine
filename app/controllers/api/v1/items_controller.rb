93c309c3cb87e802b4691b1eedc3b30e85066799
Angela Guardia <angelaguardia20@gmail.com>
2020-10-20 20:05:28 -0700

    Add strong params and create, update, destroy actions to ItemsController

--- app/controllers/api/v1/items_controller.rb
+++ app/controllers/api/v1/items_controller.rb
@@ -1,9 +1,28 @@
 class Api::V1::ItemsController < ApplicationController
   def index
-    render json: Item.all
+    render json: ItemSerializer.new(Item.all)
   end
 
   def show
-    render json: Item.find(params[:id])
+    render json: ItemSerializer.new(Item.find(params[:id]))
+  end
+
+  def create
+    Item.reset_pk_sequence
+    render json: ItemSerializer.new(Item.create!(item_params))
+  end
+
+  def update
+    render json: ItemSerializer.new(Item.update(params[:id], item_params))
+  end
+
+  def destroy
+    Item.destroy(params[:id])
+    head :no_content
+  end
+
+  private
+  def item_params
+    params.permit(:name, :description, :unit_price, :merchant_id)
   end
 end
