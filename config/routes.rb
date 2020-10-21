be5b58c6ccb9d05bdaaf1b143c7f71d8d9a8bac0
Angela Guardia <angelaguardia20@gmail.com>
2020-10-21 12:05:30 -0700

    Add single_finder route for merchant

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
       resources :items
+      get '/merchants/find', to: 'merchants/search#show'
       resources :merchants do
         get :items, to: 'merchants/items#index'
       end
