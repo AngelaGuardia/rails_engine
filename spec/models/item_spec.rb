6c92f5ca89271ea7202e24053228fca5af4b6316
Angela Guardia <angelaguardia20@gmail.com>
2020-10-21 15:19:57 -0700

    Add test for multi_finder in Item model test

--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -43,5 +43,45 @@ RSpec.describe Item, type: :model do
       # result = Item.single_finder({ updated_at: DateTime.now })
       # expect(result).to be_an(Item)
     end
+
+    it '#multi_finder' do
+      merchant1 = create(:merchant)
+      merchant2 = create(:merchant)
+      item1 = Item.create(name: "Golf club", description: "The most powerful club ever", unit_price: 109.89, merchant: merchant1)
+      item2 = Item.create(name: "Club Soda", description: "Best drink you have ever tasted", unit_price: 50.89, merchant: merchant2)
+      item3 = Item.create(name: "Soda can", description: "It's a can", unit_price: 50.89, merchant: merchant2)
+      item4 = Item.create(name: "Soda shop", description: "welcome to the soda shop", unit_price: 60, merchant: merchant2)
+
+      result = Item.multi_finder({ name: 'Golf' })
+      expect(result.first).to eq(item1)
+
+      result = Item.multi_finder({ name: 'Soda' })
+      result.each do |item|
+        expect([item2, item3, item4].include? item).to be_truthy
+      end
+
+      result = Item.multi_finder({ name: 'club' })
+      result.each do |item|
+        expect([item1, item2].include? item).to be_truthy
+      end
+
+      result = Item.multi_finder({ description: 'ever' })
+      result.each do |item|
+        expect([item1, item2].include? item).to be_truthy
+      end
+
+      result = Item.multi_finder({ unit_price: 50.89 })
+      result.each do |item|
+        expect([item3, item2].include? item).to be_truthy
+      end
+
+      result = Item.multi_finder({ merchant_id: merchant2.id })
+      result.each do |item|
+        expect([item2, item3, item4].include? item).to be_truthy
+      end
+
+      #created_at
+      #updated_at
+    end
   end
 end
