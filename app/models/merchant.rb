0c6c939adc9a206573addd20164c40a36ab7fc3a
Angela Guardia <angelaguardia20@gmail.com>
2020-10-21 11:35:59 -0700

    Create single_finder Merchant class method

--- app/models/merchant.rb
+++ app/models/merchant.rb
@@ -3,4 +3,8 @@ class Merchant < ApplicationRecord
   has_many :invoices, :dependent => :delete_all
 
   validates_presence_of :name
+
+  def self.single_finder(params)
+    Merchant.where("LOWER(merchants.name) LIKE LOWER('%#{params[:name]}%')").limit(1)
+  end
 end
