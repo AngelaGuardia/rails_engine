1733e58b25755a81223944033a07375d960be2f9
Angela Guardia <angelaguardia20@gmail.com>
2020-10-21 14:31:15 -0700

    Add Item single_finder class method

--- app/models/item.rb
+++ app/models/item.rb
@@ -6,4 +6,14 @@ class Item < ApplicationRecord
 
   validates_presence_of :name, :description, :unit_price
   validates_numericality_of :unit_price, greater_than: 0
+
+  def self.single_finder(params)
+    params.each do |attribute, value|
+      if ["name", "description"].include? attribute.to_s
+        return Item.where("LOWER(items.#{attribute}) LIKE LOWER('%#{value}%')").limit(1).first
+      else
+        return Item.where("items.#{attribute} = #{value}").limit(1).first
+      end
+    end
+  end
 end
