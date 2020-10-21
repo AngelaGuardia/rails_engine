a65345bd9c517b72e4bbfe0555263a86c7c6f25d
Angela Guardia <angelaguardia20@gmail.com>
2020-10-21 14:31:04 -0700

    Add test for Item single_finder class method

--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -14,4 +14,34 @@ RSpec.describe Item, type: :model do
     it { should have_many(:invoices).through(:invoice_items) }
     it { should have_many(:transactions).through(:invoices) }
   end
+
+  describe 'class methods' do
+    it '#single_finder' do
+      merchant1 = create(:merchant)
+      merchant2 = create(:merchant)
+      item1 = Item.create(name: "Golf club", description: "The most powerful club ever", unit_price: 109.89, merchant: merchant1)
+      item2 = Item.create(name: "Club Soda", description: "Best drink you have ever tasted", unit_price: 50.89, merchant: merchant2)
+
+      result = Item.single_finder({ name: 'Golf' })
+      expect(result).to eq(item1)
+
+      result = Item.single_finder({ name: 'club' })
+      expect(result).to be_an(Item)
+
+      result = Item.single_finder({ description: "drink" })
+      expect(result).to eq(item2)
+
+      result = Item.single_finder({ unit_price: 50.89 })
+      expect(result).to eq(item2)
+
+      result = Item.single_finder({ merchant_id: merchant1.id })
+      expect(result).to eq(item1)
+
+      # result = Item.single_finder({ created_at: DateTime.now })
+      # expect(result).to be_an(Item)
+      #
+      # result = Item.single_finder({ updated_at: DateTime.now })
+      # expect(result).to be_an(Item)
+    end
+  end
 end
