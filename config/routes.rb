ebd86aee705425b5a04947457e8d23fe1fa34963
Angela Guardia <angelaguardia20@gmail.com>
2020-10-21 15:21:20 -0700

    Add route for multi_finder Items request

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
 
+      get '/items/find_all', to: 'items/search#index'
       get '/items/find', to: 'items/search#show'
       resources :items do
         get :merchants, to: 'items/merchants#show'
