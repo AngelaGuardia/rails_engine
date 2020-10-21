5a9d44792c43dfc47c73c1939a21002938609f14
Angela Guardia <angelaguardia20@gmail.com>
2020-10-21 12:36:37 -0700

    Add index action to Merchant SearchController for multifinder requests

--- app/controllers/api/v1/merchants/search_controller.rb
+++ app/controllers/api/v1/merchants/search_controller.rb
@@ -1,4 +1,8 @@
 class Api::V1::Merchants::SearchController < ApplicationController
+  def index
+    render json: MerchantSerializer.new(Merchant.multi_finder(params))
+  end
+
   def show
     render json: MerchantSerializer.new(Merchant.single_finder(params))
   end
