753c399c98fffe11b318e0c92e810b3a298b0145
Angela Guardia <angelaguardia20@gmail.com>
2020-10-22 14:54:30 -0700

    Add most_items action to merchants BI controller

--- app/controllers/api/v1/merchants/business_intelligence_controller.rb
+++ app/controllers/api/v1/merchants/business_intelligence_controller.rb
@@ -2,4 +2,8 @@ class Api::V1::Merchants::BusinessIntelligenceController < ApplicationController
   def most_revenue
     render json: MerchantSerializer.new(Merchant.most_revenue(params))
   end
+
+  def most_items
+    render json: MerchantSerializer.new(Merchant.most_items(params))
+  end
 end
