cada4efc89040f5dc77297ed9a32b0abc9cd123c
Angela Guardia <angelaguardia20@gmail.com>
2020-10-22 17:38:19 -0700

    Correct typo in invoice_item model

--- app/models/invoice_item.rb
+++ app/models/invoice_item.rb
@@ -1,7 +1,7 @@
 class InvoiceItem < ApplicationRecord
   belongs_to :item
   belongs_to :invoice
-  has_many :transations, through: :invoices
+  has_many :transactions, through: :invoice
 
   validates_presence_of :quantity, :unit_price
   validates_numericality_of :quantity, :unit_price, greater_than: 0
