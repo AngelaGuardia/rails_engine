c19debc3ea4f9026005da82d851660c77c8f1e0a
Angela Guardia <angelaguardia20@gmail.com>
2020-10-22 17:36:31 -0700

    Add revenue_over_range action to BI controller

--- app/controllers/api/v1/merchants/business_intelligence_controller.rb
+++ app/controllers/api/v1/merchants/business_intelligence_controller.rb
@@ -6,4 +6,8 @@ class Api::V1::Merchants::BusinessIntelligenceController < ApplicationController
   def most_items
     render json: MerchantSerializer.new(Merchant.most_items(params))
   end
+
+  def revenue_over_range
+    render json: RevenueSerializer.revenue_over_range(Merchant.revenue_over_range(params))
+  end
 end
